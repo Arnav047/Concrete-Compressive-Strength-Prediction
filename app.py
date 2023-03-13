ff2a05128002d69a78196a6debd8e22e2b9af6ed
Arnav Singhal <2020.arnav.singhal@ves.ac.in>
2023-03-13 15:51:18 +0530

    Docker and github actions changes commit

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ model=pickle.load(open("model.pkl","rb"))
 
 @app.route('/')
 def home():
-    return render_template('index1.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     float_features=[float(x) for x in request.form.values()]
     final_features=[np.array(float_features)]
     prediction=model.predict(final_features)
-    return render_template('index1.html',prediction_text="Cement Strength {}".format(prediction))
+    return render_template('index.html',prediction_text="Cement Strength {}".format(prediction))
 
 if __name__=='__main__':
     app.run(debug=True)
